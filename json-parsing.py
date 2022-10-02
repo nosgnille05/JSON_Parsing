62f7b3995c5d6bcf75b56cd20793237b0233f0da
nosgnille05 <tannerdamian99@gmail.com>
2022-10-01 18:11:54 -0700

    Update json-parsing.py

--- json-parsing.py
+++ json-parsing.py
@@ -1,1 +1,8 @@
-print('Hello and Welcome to JSON Parsing with Python!')
+import json
+import requests
+
+print('Hello and Welcome to JSON Parsing with Python!\n')
+
+url = requests.get('https://api.wheretheiss.at/v1/satellites/25544')
+
+print(url.json())

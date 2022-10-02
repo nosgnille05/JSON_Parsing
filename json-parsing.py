e1b51cb3e36256a676683397df4f66b056e6e3dd
nosgnille05 <tannerdamian99@gmail.com>
2022-10-01 19:40:19 -0700

    Update json-parsing.py

--- json-parsing.py
+++ json-parsing.py
@@ -1,4 +1,5 @@
 import json
+from statistics import mean
 import requests
 import pprint
 import sys
@@ -7,28 +8,20 @@ import time
 def km_to_mile(kilometers: float) -> float:
     return kilometers * 0.62137
 
-def velocity_descrepancy(url) -> float:
-    print('Starting Descrepancy Calculator . . . (approx. 10 sec)')
+def avg_velocity(url) -> float:
+    print('Starting Avg Velocity Calculator . . .\nGathering Data (approx. 10 sec) . . .')
+    velocities = []
     for x in range (0, 5):
-        #do stuff
-        print(f'x: {x}')
-        x += 1
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            velocity = data['velocity']
+            velocities.append(velocity)
+        else:
+            sys.stderr.write(f'Error: {response.status_code}')
+            exit(1)
         time.sleep(2)
-    """
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        #pprint.pprint(data)
-        name = data['name']
-        timestamp = data['timestampstamp']
-        altitude = data['altitude']
-    else:
-        sys.stderr.write(f'Error: {response.status_code}')
-        exit(1)
-    """
-
-
-print('Hello from json-parsing.py')
+    return mean(velocities)
 
 if __name__ == "__main__":
     print('Hello and Welcome to JSON Parsing with Python!\n')
@@ -38,18 +31,20 @@ if __name__ == "__main__":
 
     if response.status_code == 200:
         data = json.loads(response.text)
-        #pprint.pprint(data)
+        pprint.pprint(data)
         name = data['name']
         timestamp = data['timestamp']
         altitude = data['altitude']
         velocity = data['velocity']
-        print(f'Information in (km) for the {name}\n        time:    {timestamp}\n    Altitude: {altitude} (km)\n    Velocity: {velocity} (km/hr)\n')
+        print(f'\nInformation in (km) for the {name}\n        time:    {timestamp}\n    Altitude: {altitude} (km)\n    Velocity: {velocity} (km/hr)\n')
         print('Lets convert to miles!\n')
         altitude = km_to_mile(altitude)
         velocity = km_to_mile(velocity)
-        print(f'Information in (miles) for the {name}\n        time:    {timestamp}\n    Altitude: {altitude} (miles)\n    Velocity: {velocity} (miles/hr)\n')
+        print(f'Information in (mi) for the {name}\n        time:    {timestamp}\n    Altitude: {altitude} (mi)\n    Velocity: {velocity} (mi/hr)\n')
     else:
         sys.stderr.write(f'Error: {response.status_code}')
         exit(1)
 
-    velocity_descrepancy(url)
+    avg_velocity = avg_velocity(url)
+    print(f'ISS Average Velocity (km/hr): {avg_velocity}')
+    print(f'ISS Average Velocity (mi/hr): {km_to_mile(avg_velocity)}')
